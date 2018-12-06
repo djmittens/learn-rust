c26878528cc47d5e4677e649b9d5cb33c5e22d29
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2018-12-05 16:47:24 -0800

    solving for factors

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// Aparantly you only need to put this stuff in the main file (extern crates that is)
+extern crate primal;
+
+mod day2;
+
+fn main() {
+    println!("Hello, world!");
+    // Day 2 example
+    // println!("{:?}", day2::num_divisors(2610, &day2::sieve()));
+}
