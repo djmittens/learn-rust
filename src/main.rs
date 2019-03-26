38b674dd1db9392de28a733a12461e9ec824b4d7
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2019-03-26 16:37:06 -0700

    adding more stuff about rust

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ mod vars;
 
 use std::thread;
 use std::time::Duration;
+use std::collections::HashMap;
+use std::hash::Hash;
 
 fn main() {
     if false {
@@ -101,20 +103,43 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| {
+    let mut expensive_closure = Cacher::new(|num| {
         println!("calculating slowly ...");
         thread::sleep(Duration::from_secs(2));
         intensity
-    };
+    });
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_closure(intensity));
-        println!("Next, do {} situps !", expensive_closure(intensity));
+        println!("Today, do {} pushups!", expensive_closure.value(intensity));
+        println!("Next, do {} situps !", expensive_closure.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes!", expensive_closure(intensity));
+            println!("Today, run for {} minutes!", expensive_closure.value(intensity));
         }
+    }
+}
+
+struct Cacher <T, U, Z>
+    where T: Fn(U) -> Z, U: Eq + Hash
+{
+    calculation: T,
+    map: HashMap<U, Z>,
+}
+
+impl<T, U, Z> Cacher<T, U, Z> where T: Fn(U) -> Z, U: Eq + Hash + Clone {
+    fn new(calculation: T) -> Cacher<T, U, Z> {
+        Cacher {
+            calculation,
+            map: HashMap :: new(),
+        }
+    }
+
+    fn value(&mut self, arg: U) -> & Z {
+        let calc = &self.calculation;
+        let map = &mut self.map;
+        let a = arg.clone();
+        map.entry(a).or_insert_with(||{calc(arg.clone())})
     }
 }
 
