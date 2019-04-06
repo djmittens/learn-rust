7aa8dd7ae3c7df09b4e25b9b8c6af2ccb7fef152
Nikita Gridinskiy <nikita.gridinskiy@gmail.com>
2019-04-06 16:55:47 -0700

    finally done with the rust book woo

---
+++ rust-language-book/servio/src/main.rs
@@ -0,0 +1,48 @@
+use servio::ThreadPool;
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(4) {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+    println!("Shutting down");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    //what is this syntax ???
+    let mut buffer = [0; 512];
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    stream.read(&mut buffer).unwrap();
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(15));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 OK\r\n\r\n", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
