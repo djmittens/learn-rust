64b928f809ceadb5060eb35b879f142150945138
Nikita Gridinskiy <nikita.gridinskiy@gmail.com>
2019-04-28 12:48:22 -0700

    Some more work in progress or something like that.

--- cargo-hal/bookofshaders/chapter05.glsl
+++ cargo-hal/bookofshaders/chapter05.glsl
@@ -6,17 +6,17 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-float plot(vec2 st) {
-    return smoothstep(0.02, 0., abs(st.y - st.x));
+float plot(vec2 st, float pct) {
+    return smoothstep(pct - 0.01, pct, st.y) - smoothstep(pct, pct + 0.01, st.y);
 }
 
 void main() {
     vec2 xy = gl_FragCoord.xy / u_resolution;
-    float y = pow(xy.x, 5.0);
+    float y = pow(xy.x, 2.);
     vec3 color = vec3(y);
 
     // Plot a line
-    float pct  = plot(xy);
+    float pct  = plot(xy,y);
     color = (1. - pct) * color + pct * vec3(0., 1., 0.);
 
     gl_FragColor = vec4(color, 1.);
